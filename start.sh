7918e1f537e0f63ac8c67c1797bd4276617c1ae0
jzarris-sigsci <45439489+jzarris-sigsci@users.noreply.github.com>
2020-04-07 16:22:05 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 /bin/bash -c "envsubst '\$PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf" && nginx -g 'daemon off;' &
-/usr/sbin/sigsci-agent
+/usr/sbin/sigsci-agent &
+
